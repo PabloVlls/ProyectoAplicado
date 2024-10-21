0491a78ad3bf699092fa0c193b1d8aa6a5a41214
Pablo <pabloroblefra@gmail.com>
2024-10-20 23:50:35 -0500

    LevelLoader
    IMPORTANTE (CAMBIE VARIAS VECES LAS BUILD SETINGS (La build a android y scenes))
    
    Loadlevels 3 scenas

---
+++ ProyectoAplicadoPet/Assets/Scripts/BrickBreaker/BB_GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BB_GameManager : MonoBehaviour
+{
+    public int level = 1;
+    public int score = 0;
+    public int lives = 3;
+
+    private void Awake()  
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        this.score = 0;
+        this.lives = 3;
+        LoadLevel(1);
+    }
+
+    private void LoadLevel(int level)
+    {
+        this.level = level;
+
+       SceneManager.LoadScene("Level " + level);
+    }
+
+}
